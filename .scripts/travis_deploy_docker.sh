fe8df93f3ff5e653b2a293b5a4fc7b69757789b5
Toni Karppi <karppitoni@gmail.com>
2019-04-27 14:08:39 +0200

    Reorder docker pushes
    Leads to nicer output in docker hub

--- .scripts/travis_deploy_docker.sh
+++ .scripts/travis_deploy_docker.sh
@@ -5,7 +5,7 @@ set -e
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 export PRETTY_COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty=%h)
 docker tag $IMG:latest $IMG:$PRETTY_COMMIT
-docker push $IMG:builder
-docker push $IMG:tester
 docker push $IMG:$PRETTY_COMMIT
+docker push $IMG:tester
+docker push $IMG:builder
 docker push $IMG:latest
