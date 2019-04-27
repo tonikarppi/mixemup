dd639c3f8e7f7716d6c324198265da0a82e3bd18
Toni Karppi <karppitoni@gmail.com>
2019-04-27 15:46:48 +0200

    Reorder docker push to push builder first

--- .scripts/travis_deploy_docker.sh
+++ .scripts/travis_deploy_docker.sh
@@ -7,6 +7,6 @@ IMG=tonikarppi/mixemup
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 PRETTY_COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty=%h)
 docker tag $IMG:latest $IMG:$PRETTY_COMMIT
-docker push $IMG:$PRETTY_COMMIT
 docker push $IMG:builder
 docker push $IMG:latest
+docker push $IMG:$PRETTY_COMMIT
