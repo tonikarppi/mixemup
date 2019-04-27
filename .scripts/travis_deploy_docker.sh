3720dd67e66aa364677d8ebe667156e1dede324c
Toni Karppi <karppitoni@gmail.com>
2019-04-27 15:30:23 +0200

    Remove testing img push from deploy script

--- .scripts/travis_deploy_docker.sh
+++ .scripts/travis_deploy_docker.sh
@@ -6,6 +6,5 @@ echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 PRETTY_COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty=%h)
 docker tag $IMG:latest $IMG:$PRETTY_COMMIT
 docker push $IMG:$PRETTY_COMMIT
-docker push $IMG:tester
 docker push $IMG:builder
 docker push $IMG:latest
